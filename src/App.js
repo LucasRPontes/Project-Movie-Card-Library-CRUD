894a2d0b126b295f3833666735567f83e6f73b8c
Lucas Rodrigues <lucasrpontesgarcia@gmail.com>
2021-05-16 22:20:31 -0300

    ajuste lint

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch
-  , Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import EditMovie from './pages/EditMovie';
 import MovieDetails from './pages/MovieDetails';
