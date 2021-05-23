a652ff37881f29861a2dc3d50c8d535ca90c7bb8
Lucas Rodrigues <lucasrpontesgarcia@gmail.com>
2021-05-23 19:02:48 -0300

    datatestid moviecard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -15,7 +15,7 @@ class MovieCard extends React.Component {
     } } = this.props;
 
     return (
-      <div className="movie-card movie-card-body">
+      <div data-testid="movie-card" className="movie-card movie-card-body">
         <img className="movie-card-image" src={ imagePath } alt={ title } />
         <h4 className="movie-card-title">{ title }</h4>
         <h5 className="movie-card-subtitle">{ subtitle }</h5>
